ad3d902bc46b6479bcd8f6a6f53cf149d1b11e0d
Truls Rustad Fossum <trulfos@gmail.com>
2017-01-11 19:34:59 +0100

    Inline SVG icons
    Shorter load times and easier styling from the theme

--- Widget/ShareButtons/skin/default.php
+++ Widget/ShareButtons/skin/default.php
@@ -3,9 +3,7 @@
 foreach ($links as $name => $link) {
 ?>
 	<a href="<?php echo $link ?>" rel="noopener" target="_blank">
-		<img
-			src="<?php echo ipFileUrl($assets . strtolower($name) . '.svg'); ?>"
-			alt="Share on <?php echo $name; ?>">
+		<?php include($assets . strtolower($name) . '.svg') ?>
 	</a>
 <?php
 }
